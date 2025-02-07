c03ab3b56bf1edeede2a9d52fb6fe095999871b6
Cyrus Malekani <cyrus.malekani@gmail.com>
2025-02-07 21:48:53 +0000

    Front page.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FrontPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FrontPage from "./components/FrontPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FrontPage,
+  },
+};
 </script>
 
 <style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    host: "70.34.195.242",
+    port: 8080,
+    allowedHosts: "all", // Allow all hosts (or specify a domain)
+  }
+};
